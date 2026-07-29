7ea119f36986b7a52ef176d902ae121fa4e94c15
jebez sharon <jebezsharon453@gmail.com>
2026-07-29 11:16:32 +0530

    Add youtube video downloader at high res

---
+++ Youtube_video_downloader/youtube_downloader.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import filedialog
+import yt_dlp
+
+def download_video(url, save_path):
+    try:
+        ydl_opts = {
+            "format": "bestvideo+bestaudio/best",
+            "merge_output_format": "mp4",
+            "outtmpl": save_path + "/%(title)s.%(ext)s",
+}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Video downloaded successfully!")
+
+    except Exception as e:
+        print(e)
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
+
+    return folder
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    video_url = input("Please enter a Youtube url: ")
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("Started download...")
+        download_video(video_url, save_dir)
+    else:
+        print("Invalid save location")
+        
